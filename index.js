62b7762d96f11333a1e623e93b7f2ab81589417f
RealChoki <rafaatchoki1999@gmail.com>
2023-08-31 18:59:58 +0200

    Added Adjust time styling

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const buttons = document.querySelectorAll("button")
 
 
 firstTimer.addEventListener("click", () => {
-  timePicker.classList.remove("hidden");
+  timePicker.classList.remove("hidden")
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.2)"
   buttons.forEach(button => {
     if (button.parentElement.classList != "papa"){
         button.disabled = true;
@@ -15,7 +16,8 @@ firstTimer.addEventListener("click", () => {
 });
 
 secondTimer.addEventListener("click", () => {
-    timePicker2.classList.remove("hidden");
+  timePicker2.classList.remove("hidden")
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.2)"
    buttons.forEach(button => {
     if (button.parentElement.classList != "papa"){
         button.disabled = true;
@@ -27,32 +29,43 @@ document.addEventListener("DOMContentLoaded", function () {
   const hoursInput = document.getElementById("hours");
   const minutesInput = document.getElementById("minutes");
   const secondsInput = document.getElementById("seconds");
-
   const cancelBtn = document.getElementById("cancel");
   const saveBtn = document.getElementById("save");
+  
+  const hoursInput2 = document.getElementById("hours2");
+  const minutesInput2 = document.getElementById("minutes2");
+  const secondsInput2 = document.getElementById("seconds2");
   const cancelBtn2 = document.getElementById("cancel2");
   const saveBtn2 = document.getElementById("save2");
 
   cancelBtn.addEventListener("click", () => {
-    timePicker.classList.add("hidden");
+    timePicker.classList.add("hidden")
+    document.body.style.backgroundColor = ""
+
     buttons.forEach(button => {
         button.disabled = false;
        })
   });
   saveBtn.addEventListener("click", () => {
-    timePicker.classList.add("hidden");
+    timePicker.classList.add("hidden")
+    document.body.style.backgroundColor = ""
+
     buttons.forEach(button => {
         button.disabled = false;
        })
   });
   cancelBtn2.addEventListener("click", () => {
-    timePicker2.classList.add("hidden");
+    timePicker2.classList.add("hidden")
+    document.body.style.backgroundColor = ""
+
     buttons.forEach(button => {
         button.disabled = false;
        })
   });
   saveBtn2.addEventListener("click", () => {
-    timePicker2.classList.add("hidden");
+    timePicker2.classList.add("hidden")
+    document.body.style.backgroundColor = ""
+
     buttons.forEach(button => {
         button.disabled = false;
        })
