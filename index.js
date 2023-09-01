5803f31164fd48f07d936ab359d056e75b02da94
RealChoki <rafaatchoki1999@gmail.com>
2023-09-01 13:30:12 +0200

    connect time / count moves / hide elements

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const firstTimer = document.getElementById("change-time");
-const secondTimer = document.getElementById("sec-change-time");
-const timePicker = document.querySelector(".adjust-time");
-const timePicker2 = document.querySelector(".adjust-time2");
+const firstTimer = document.getElementById("change-time")
+const secondTimer = document.getElementById("sec-change-time")
+const timePicker = document.querySelector(".adjust-time")
+const timePicker2 = document.querySelector(".adjust-time2")
 const buttons = document.querySelectorAll("button")
 
 
@@ -13,7 +13,7 @@ firstTimer.addEventListener("click", () => {
         button.disabled = true;
     }
    })
-});
+})
 
 secondTimer.addEventListener("click", () => {
   timePicker2.classList.remove("hidden")
@@ -23,56 +23,83 @@ secondTimer.addEventListener("click", () => {
         button.disabled = true;
     }
    })
-});
+})
 
 document.addEventListener("DOMContentLoaded", function () {
-  const hoursInput = document.getElementById("hours");
-  const minutesInput = document.getElementById("minutes");
-  const secondsInput = document.getElementById("seconds");
-  const cancelBtn = document.getElementById("cancel");
-  const saveBtn = document.getElementById("save");
+  const cancelBtn = document.getElementById("cancel")
+  const saveBtn = document.getElementById("save")
+  const firstClock = document.getElementById("clock")
+  let counter1 = 0
+  let counter2 = 0
+
+  document.querySelector(".first-tap").addEventListener("click", e => {
+    if (e.target.parentElement.id != "change-time") {
+      counter1++
+      document.getElementById("counter1").textContent = counter1
+      
+      document.querySelectorAll(".hide").forEach(element => {
+        element.classList.add("hidden")
+      }) 
+    }
+  })
   
-  const hoursInput2 = document.getElementById("hours2");
-  const minutesInput2 = document.getElementById("minutes2");
-  const secondsInput2 = document.getElementById("seconds2");
-  const cancelBtn2 = document.getElementById("cancel2");
-  const saveBtn2 = document.getElementById("save2");
+  document.querySelector(".second-tap").addEventListener("click", e => {
+    if (e.target.parentElement.id != "sec-change-time") {
+      counter2++
+      document.getElementById("counter2").textContent = counter2
+      
+      document.querySelectorAll(".hide").forEach(element => {
+        element.classList.add("hidden")
+      })  
+    }
+  })
 
   cancelBtn.addEventListener("click", () => {
     timePicker.classList.add("hidden")
     document.body.style.backgroundColor = ""
-
     buttons.forEach(button => {
         button.disabled = false;
        })
-  });
+  })
   saveBtn.addEventListener("click", () => {
+    const hoursInput = document.getElementById("hours").value
+    const minutesInput = document.getElementById("minutes").value
+    const secondsInput = document.getElementById("seconds").value
+    updateClock(firstClock, hoursInput, minutesInput, secondsInput)
     timePicker.classList.add("hidden")
     document.body.style.backgroundColor = ""
-
     buttons.forEach(button => {
         button.disabled = false;
        })
-  });
+  })
+  const cancelBtn2 = document.getElementById("cancel2")
+  const saveBtn2 = document.getElementById("save2")
+  const secondClock = document.getElementById("sec-clock")
+  
   cancelBtn2.addEventListener("click", () => {
     timePicker2.classList.add("hidden")
     document.body.style.backgroundColor = ""
-
     buttons.forEach(button => {
         button.disabled = false;
        })
-  });
+  })
   saveBtn2.addEventListener("click", () => {
+    const hoursInput2 = document.getElementById("hours2").value
+    const minutesInput2 = document.getElementById("minutes2").value
+    const secondsInput2 = document.getElementById("seconds2").value
+    updateClock(secondClock, hoursInput2, minutesInput2, secondsInput2)
     timePicker2.classList.add("hidden")
     document.body.style.backgroundColor = ""
-
     buttons.forEach(button => {
         button.disabled = false;
        })
-  });
+  })
+})
 
-
-});
+function updateClock(clockElement, hours, minutes, seconds) {
+  const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  clockElement.textContent = formattedTime;
+}
 
 // david
 const volumeBtn = document.getElementById("volume-btn");
