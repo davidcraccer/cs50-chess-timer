8b1c4ea62ceb1352c891a8b5e812ad8c4d16ccbd
davidcraccer <dsvoboda25@gmail.com>
2023-09-06 14:33:50 +0200

    Code optimized + Clock takes inputs

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Get references to various HTML elements using their IDs and classes.
 import Clock from "/Clock.js";
 
-const timerSettingBtn = document.getElementById("change-time");
-const timerSettingBtn2 = document.getElementById("sec-change-time");
 const timerSetting = document.querySelector(".adjust-time");
 const timerSetting2 = document.querySelector(".adjust-time2");
+const timerSettingBtn = document.getElementById("change-time");
+const timerSettingBtn2 = document.getElementById("sec-change-time");
 const buttons = document.querySelectorAll("button");
+// Anchor tags cant be disabled with disabled = true
+const anchors = document.querySelectorAll("a");
 const clickSound = document.getElementById("click-sound");
+
+// Clocks setted with the default values
+const secondClock = document.getElementById("clock2");
+let clock2 = new Clock(0, 10, 0, secondClock);
+const firstClock = document.getElementById("clock");
+let clock1 = new Clock(0, 10, 0, firstClock);
 
 let isGameOn = false;
 let disableGame = false;
@@ -15,9 +23,6 @@ let firstPlayerTurn = true;
 let moveCounter1 = 0;
 let moveCounter2 = 0;
 let startGameMoveCounter = 0;
-
-// TODO: create new two clocks here and update its values when clicked on the first time
-// let clock1, clock2;
 
 // Execute code when the DOM is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
@@ -35,12 +40,40 @@ document.addEventListener("DOMContentLoaded", function () {
     .querySelector(".second-tapping-field")
     .addEventListener("click", secondTapClickHandler);
 
-  const cancelBtn = document.getElementById("cancel");
-  const saveBtn = document.getElementById("save");
-  const firstClock = document.getElementById("clock");
+  initializeTimerEvents(
+    clock1,
+    "cancel",
+    "save",
+    timerSetting,
+    "hours",
+    "minutes",
+    "seconds"
+  );
+  initializeTimerEvents(
+    clock2,
+    "cancel2",
+    "save2",
+    timerSetting2,
+    "hours2",
+    "minutes2",
+    "seconds2"
+  );
+});
 
+function initializeTimerEvents(
+  clock,
+  cancel,
+  save,
+  timerSetting,
+  hours,
+  minutes,
+  seconds
+) {
+  const cancelBtn = document.getElementById(cancel);
+  const saveBtn = document.getElementById(save);
   // Add event listeners for the "cancel" and "save" buttons for the first timer.
   cancelBtn.addEventListener("click", () => {
+    console.log("cancel");
     timerSetting.classList.add("hidden");
     document.body.style.backgroundColor = "";
     disableGame = true;
@@ -51,135 +84,47 @@ document.addEventListener("DOMContentLoaded", function () {
   // Add a click event listener to the save button
   saveBtn.addEventListener("click", () => {
     // Retrieve user input for hours, minutes, and seconds.
-    const hoursInput = parseInt(document.getElementById("hours").value, 10);
-    const minutesInput = parseInt(document.getElementById("minutes").value, 10);
-    const secondsInput = parseInt(document.getElementById("seconds").value, 10);
+    const hoursInput = parseInt(document.getElementById(hours).value, 10);
+    const minutesInput = parseInt(document.getElementById(minutes).value, 10);
+    const secondsInput = parseInt(document.getElementById(seconds).value, 10);
 
     timerSetting.classList.add("hidden");
     document.body.style.backgroundColor = "";
     disableGame = true;
 
-    // Enable buttons in your code (if necessary).
     buttons.forEach((button) => {
       button.disabled = false;
     });
 
     // Update the timer with the provided input values
-    updateClock(hoursInput, minutesInput, secondsInput, firstClock);
+    clock.updateClock(hoursInput, minutesInput, secondsInput);
   });
-
-  // Similar event listeners for the second timer (cancel and save).
-  const cancelBtn2 = document.getElementById("cancel2");
-  const saveBtn2 = document.getElementById("save2");
-  const secondClock = document.getElementById("clock2");
-
-  cancelBtn2.addEventListener("click", () => {
-    timerSetting2.classList.add("hidden");
-    document.body.style.backgroundColor = "";
-    disableGame = false;
-    buttons.forEach((button) => {
-      button.disabled = false;
-    });
-  });
-
-  saveBtn2.addEventListener("click", () => {
-    const hoursInput2 = parseInt(document.getElementById("hours2").value, 10);
-    const minutesInput2 = parseInt(
-      document.getElementById("minutes2").value,
-      10
-    );
-    const secondsInput2 = parseInt(
-      document.getElementById("seconds2").value,
-      10
-    );
-
-    timerSetting2.classList.add("hidden");
-    document.body.style.backgroundColor = "";
-    disableGame = false;
-    buttons.forEach((button) => {
-      button.disabled = false;
-    });
-
-    // Update the timer with the provided input values
-    updateClock(hoursInput2, minutesInput2, secondsInput2, secondClock);
-  });
-});
-
-// Function to update the clock display with formatted time.
-function updateClock(hours, minutes, seconds, clockElement) {
-  let formattedTime;
-  if (hours == 0 && minutes == 0) {
-    formattedTime = `${String(seconds)}s`;
-  } else if (hours == 0) {
-    formattedTime = `${String(minutes).padStart(2, "0")}:${String(
-      seconds
-    ).padStart(2, "0")}`;
-  } else {
-    formattedTime = `${String(hours).padStart(2, "0")}:${String(
-      minutes
-    ).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-  }
-  clockElement.textContent = formattedTime;
 }
-
-// function startTimer(hours, minutes, seconds, clockElement) {
-//   let totalSeconds = hours * 3600 + minutes * 60 + seconds;
-
-//   const timerInterval = setInterval(() => {
-//     if (totalSeconds <= 0) {
-//       clearInterval(timerInterval);
-//       console.log("Timer expired");
-//     } else {
-//       const hoursRemaining = Math.floor(totalSeconds / 3600);
-//       const minutesRemaining = Math.floor((totalSeconds % 3600) / 60);
-//       const secondsRemaining = totalSeconds % 60;
-
-//       // Update the clock display with the formatted time
-//       updateClock(
-//         hoursRemaining,
-//         minutesRemaining,
-//         secondsRemaining,
-//         clockElement
-//       );
-
-//       totalSeconds--;
-//     }
-//   }, 1000);
-// }
 
 const volumeBtn = document.getElementById("volume-btn");
 const spanVolumeOn = document.getElementById("volume-on");
 const spanVolumeOff = document.getElementById("volume-off");
+// Track sound state
+let isVolumeOn = true;
 
-// Toggle the volume icon
-volumeBtn.addEventListener("click", () => {
-  spanVolumeOn.hidden = !spanVolumeOn.hidden;
-  spanVolumeOff.hidden = !spanVolumeOff.hidden;
+// Function to toggle volume on and off
+function toggleVolume() {
+  clickSound.volume = isVolumeOn ? 0 : 1;
+  isVolumeOn = !isVolumeOn;
+  spanVolumeOn.hidden = !isVolumeOn;
+  spanVolumeOff.hidden = isVolumeOn;
+}
+
+// Toggle the volume icon and volume control
+volumeBtn.addEventListener("click", toggleVolume);
+
+
+// Start game
+document.getElementById("play-btn").addEventListener("click", (e) => {
+  firstTapClickHandler(e);
 });
 
 function startGame() {}
-const secondClock = document.getElementById("clock2");
-
-const hoursInput2 = parseInt(document.getElementById("hours2").value, 10);
-const minutesInput2 =
-  parseInt(document.getElementById("minutes2").value, 10) || 10;
-const secondsInput2 = parseInt(document.getElementById("seconds2").value, 10);
-// Start the timer with the provided input values
-const clock2 = new Clock(
-  hoursInput2,
-  minutesInput2,
-  secondsInput2,
-  secondClock
-);
-const firstClock = document.getElementById("clock");
-
-const hoursInput = parseInt(document.getElementById("hours").value, 10);
-const minutesInput =
-  parseInt(document.getElementById("minutes").value, 10) || 10;
-const secondsInput = parseInt(document.getElementById("seconds").value, 10);
-
-// Start the timer with the provided input values
-const clock1 = new Clock(hoursInput, minutesInput, secondsInput, firstClock);
 
 // Define the event listener function for "first-tap."
 function firstTapClickHandler(e) {
@@ -187,33 +132,35 @@ function firstTapClickHandler(e) {
   if (e.target.parentElement.id !== "change-time" && firstPlayerTurn) {
     isGameOn = true;
     clickSound.play();
+
+    // Change the color of the field
     document.querySelector(".second-tapping-field").style.backgroundColor =
       "#7FA44F";
     secondClock.style.color = "white";
     firstClock.style.color = "#323232";
     document.querySelector(".first-tapping-field").style.backgroundColor = "";
+
     firstPlayerTurn = !firstPlayerTurn;
 
+    const hoursInput2 = parseInt(document.getElementById("hours2").value, 10);
+    let minutesInput2 = parseInt(document.getElementById("minutes2").value, 10);
+    const secondsInput2 = parseInt(
+      document.getElementById("seconds2").value,
+      10
+    );
+
+    if (hoursInput2 === 0 && minutesInput2 === 0 && secondsInput2 === 0) {
+      minutesInput2 = 10;
+    }
+    clock2.setClock(hoursInput2, minutesInput2, secondsInput2);
+
+    // Stops the incrementation of the first field tapped
     if (startGameMoveCounter >= 1) {
       moveCounter1++;
     }
 
     if (startGameMoveCounter < 2) {
       startGameMoveCounter++;
-
-      // Clock Information
-      // const secondClock = document.getElementById("clock2");
-
-      // const hoursInput2 = parseInt(document.getElementById("hours2").value, 10);
-      // const minutesInput2 =
-      //   parseInt(document.getElementById("minutes2").value, 10) || 10;
-      // const secondsInput2 = parseInt(
-      //   document.getElementById("seconds2").value,
-      //   10
-      // );
-      // // Start the timer with the provided input values
-      // const clock = new Clock(hoursInput2, minutesInput2, secondsInput2, secondClock)
-      // clock.start()
     }
     clock1.stop();
     clock2.start();
@@ -226,6 +173,7 @@ function firstTapClickHandler(e) {
     document
       .querySelector(".second-tapping-field")
       .addEventListener("click", secondTapClickHandler);
+
     // Hide elements with class "hide."
     document.querySelectorAll(".hide").forEach((element) => {
       element.classList.add("hidden");
@@ -239,29 +187,28 @@ function secondTapClickHandler(e) {
   if (e.target.parentElement.id !== "sec-change-time" && !firstPlayerTurn) {
     isGameOn = true;
     clickSound.play();
+
+    // update the field
     document.querySelector(".first-tapping-field").style.backgroundColor =
       "#7FA44F";
     firstClock.style.color = "white";
     secondClock.style.color = "#323232";
     document.querySelector(".second-tapping-field").style.backgroundColor = "";
+
+    const hoursInput = parseInt(document.getElementById("hours").value, 10);
+    let minutesInput = parseInt(document.getElementById("minutes").value, 10);
+    const secondsInput = parseInt(document.getElementById("seconds").value, 10);
+
+    if (hoursInput === 0 && minutesInput === 0 && secondsInput === 0) {
+      minutesInput = 10;
+    }
+
+    clock1.setClock(hoursInput, minutesInput, secondsInput);
+
     moveCounter2++;
     firstPlayerTurn = !firstPlayerTurn;
     if (startGameMoveCounter < 2) {
       startGameMoveCounter++;
-      // Clock Information
-      // const firstClock = document.getElementById("clock");
-
-      // const hoursInput = parseInt(document.getElementById("hours").value, 10);
-      // const minutesInput =
-      //   parseInt(document.getElementById("minutes").value, 10) || 10;
-      // const secondsInput = parseInt(
-      //   document.getElementById("seconds").value,
-      //   10
-      // );
-
-      // // Start the timer with the provided input values
-      // const clock = new Clock(hoursInput, minutesInput, secondsInput, firstClock)
-      // clock.start()
     }
     clock2.stop();
     clock1.start();
@@ -288,8 +235,14 @@ timerSettingBtn.addEventListener("click", () => {
   document.body.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
   disableGame = true;
   // Disable buttons except those within an element with class "papa."
+  // anchors.forEach((anchor) => {
+  //   const grandparent = anchor.parentElement.parentElement;
+  //   if (grandparent.classList.contains("papa")) {
+  //     anchor.disabled = true;
+  //   }
+  // })
   buttons.forEach((button) => {
-    if (button.parentElement.classList != "papa") {
+    if (!button.parentElement.classList.contains("papa")) {
       button.disabled = true;
     }
   });
@@ -300,8 +253,14 @@ timerSettingBtn2.addEventListener("click", () => {
   timerSetting2.classList.remove("hidden");
   document.body.style.backgroundColor = "rgba(0, 0, 0, 0.2)";
   disableGame = true;
+  // anchors.forEach((anchor) => {
+  //   const grandparent = anchor.parentElement.parentElement;
+  //   if (grandparent.classList.contains("papa")) {
+  //     anchor.disabled = true;
+  //   }
+  // })
   buttons.forEach((button) => {
-    if (button.parentElement.classList != "papa") {
+    if (!button.parentElement.classList.contains("papa")) {
       button.disabled = true;
     }
   });
