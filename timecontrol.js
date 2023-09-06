8f85a628ca17ac910d3d6824bf968ed4ef4d0c15
RealChoki <123271772+RealChoki@users.noreply.github.com>
2023-09-06 16:29:11 +0200

    Update timecontrol.js

--- timecontrol.js
+++ timecontrol.js
@@ -1,9 +1,21 @@
 
+let selectedColor = null
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("palette-btn").addEventListener("click", () =>{
         document.querySelector(".color-theme").classList.remove("hidden")
     })
     document.getElementById("save-color").addEventListener("click", () =>{
         document.querySelector(".color-theme").classList.add("hidden")
+        const radioButtons = document.getElementsByName("radioNoLabel")
+        for (let radioButton of radioButtons){
+            if (radioButton.checked){
+                console.log(radioButton.value)
+                break
+            }
+        }
     })
 })
+
+export function getSelectedColor(){
+    return selectedColor;
+}
