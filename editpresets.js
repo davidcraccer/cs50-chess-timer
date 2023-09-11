fdf31c743196ef29f4b3e5689aa3a3ed33416eb9
RealChoki <rafaatchoki1999@gmail.com>
2023-09-11 19:17:32 +0200

    Added deltete presets functionality

--- editpresets.js
+++ editpresets.js
@@ -1,7 +1,41 @@
 //? Im repeating this too much gotta do smth about it 
+
 let selectedColor = localStorage.getItem("selectedColor")
+let customTimes = JSON.parse(localStorage.getItem("customTimes")) || []
+
 document.documentElement.style.setProperty("--selectedColor3", selectedColor)
 
+// Feed all times in form-check
+for (let customtime of customTimes){
+    document.querySelector(".form-check").innerHTML += `
+        <div class="presets-box">
+            <div class="presets-childs">
+                <input type="checkbox" value="" id="${customtime.time}" name="radio-group" class="preset-check form-check-input">
+                <label class="check-label" for="${customtime.time}">${customtime.label}</label><br>
+            </div>
+            <div class="presets-childs">
+                <span class="material-symbols-outlined">
+                    edit
+                </span>
+                <span class="material-symbols-outlined">
+                    drag_handle
+                </span>
+            </div>
+        </div>
+    `
+}
+
+// Delete a selected time from localstorage
 document.getElementById("delete-btn").addEventListener("click", ()=>{
-    console.log(selectedColor)
+    document.querySelectorAll(".form-check-input").forEach(input => {
+        if (input.checked){
+            customTimes.forEach((customtime, index) =>{
+                if (customtime.time == input.id){
+                    customTimes.splice(index, 1)
+                }
+            })
+        }
+    })
+    localStorage.setItem("customTimes", JSON.stringify(customTimes))
+    window.location.reload()
 })
